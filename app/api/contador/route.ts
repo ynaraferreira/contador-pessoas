bdf0eec8b62d7f2daca48cd0cfcc6cfb11112f54
Ynara Ferreira <ynaralopes@outlook.com>
2025-12-01 23:21:49 -0300

    att nova, conserto api

--- app/api/contador/route.ts
+++ app/api/contador/route.ts
@@ -1,10 +1,11 @@
+export const runtime = "nodejs";
+
 import { put, list } from "@vercel/blob";
 import { NextResponse } from "next/server";
 
 const FILE_NAME = "contador.json";
 
 async function loadContador() {
-  // O prefix precisa ser passado
   const arquivos = await list({ prefix: "" });
 
   const item = arquivos.blobs.find((b) => b.pathname === FILE_NAME);
@@ -15,7 +16,6 @@ async function loadContador() {
   return res.json();
 }
 
-// GET – retorna o contador atual
 export async function GET() {
   try {
     const data = await loadContador();
@@ -25,7 +25,6 @@ export async function GET() {
   }
 }
 
-// POST – salva novo contador
 export async function POST(req: Request) {
   try {
     const body = await req.json();
